7b28bedce20f1d88ac8cc964a7b9ae30e9fa28f0
Enrico <enricocavaliere56@gmail.com>
2026-03-20 11:16:23 -0300

    melhorando js e css

--- lista/lista.js
+++ lista/lista.js
@@ -1,25 +1,28 @@
-const adicionar = document.getElementById('adicionar');
-let input = document.getElementById("input")
-
-adicionar.addEventListener("click", () => {
-    if(input.value.trim() ===""){
-        alert("Digite uma tarefa para adicionar a sua lista")
-        return
-    }
-
-    const item = document.createElement("li");
-    item.textContent = input.value;
-
-    const excluir = document.createElement("button");
-    excluir.textContent = "Excluir item"
-
-    item.appendChild(excluir);
-    excluir.addEventListener("click", () => {
-        item.remove()
-    })
-
-    document.getElementById("lista").appendChild(item);
-
-    input.value = "";
+const btnAdicionar = document.getElementById("Adicionar");
+let input = document.getElementById("input");
+ 
+btnAdicionar.addEventListener("click", () => {
+  if(input.value.trim() === ""){
+    alert("Digite uma tarefa para adicionar.")
+    return
+  }
+ 
+  const item = document.createElement("li");
+  item.textContent = input.value;
+ 
+  const excluir = document.createElement("button");
+  excluir.textContent = "Excluir item"
+ 
+  item.appendChild(excluir);
+ 
+  excluir.addEventListener("click", () => {
+    item.remove()
+  })
+ 
+  document.getElementById("lista").appendChild(item);
+ 
+  input.value = "";
 });
-    
+ 
+ 
+ 
